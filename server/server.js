43be220e440bc923a36c9f7d6e8d1e44286d9a1f
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-04-09 19:55:20 -0400

    add boiler plate code and create folder structure for server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const path = require('path');
+const db = require('./config/connection.js');
+
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // send index.html for any routes that don't exist
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+// Will catch any requests (Get, Post, etc...) to non-specified routes
+app.all('*', (req, res, next) => {
+  res.status(404).send(`Can't find ${req.originalUrl} on this server`);
 });
 
-app.listen(3001, () => {
-  'Listening on port 3001';
+db.once('open', () => {
+  app.listen(PORT, () =>
+    console.log(`🌍 Now listening on http://localhost:${PORT}`)
+  );
 });
